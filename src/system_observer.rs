333dba1cd25dbfc98ba0fb3a80fc82790ecd4b6c
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2023-05-15 04:21:56 +0200

    Implemented tests for system_observer.rs

--- src/system_observer.rs
+++ src/system_observer.rs
@@ -48,3 +48,47 @@ fn read_stats() -> SystemStatus {
     };
     return system_status;
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_stats() {
+        let stats = read_stats();
+        assert_ne!(stats.governor, 0); // Test governor value
+        assert_ne!(stats.temp1, 0); // Test temp1 value
+        assert_ne!(stats.freq1, 0); // Test freq1 value
+        assert_ne!(stats.gpu_temp, ""); // Test gpu_temp value
+        assert_ne!(stats.zzz, 0); // Test zzz value
+    }
+
+    #[actix_rt::test]
+    async fn test_start() {
+        let test_poll_rate = 1;
+        let config = Config { poll_rate: test_poll_rate };
+        let data = start(config);
+
+        // Wait for a couple of poll cycles
+        thread::sleep(Duration::from_secs(test_poll_rate*2));
+
+        let locked_data = data.lock().unwrap();
+
+        // Access the fields of SystemStatus within the locked scope
+        let governor = locked_data.governor;
+        let temp1 = locked_data.temp1;
+        let freq1 = locked_data.freq1;
+        let gpu_temp = locked_data.gpu_temp.clone();
+        let zzz = locked_data.zzz;
+
+        drop(locked_data); // Release the lock
+
+        // Check that the SystemStatus fields have been updated
+        assert_ne!(governor, 0);
+        assert_ne!(temp1, 0);
+        assert_ne!(freq1, 0);
+        assert_ne!(gpu_temp, "");
+        assert_ne!(zzz, 0);
+    }
+}
