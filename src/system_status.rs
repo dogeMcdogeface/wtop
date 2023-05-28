345a8a4ac6beac7def7ae5b364c27b33baca3158
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2023-05-28 21:49:29 +0200

    Updated type names to be more clear

--- src/system_status.rs
+++ src/system_status.rs
@@ -1,16 +1,16 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-//------------------------------------------------------------------------------------------------//
+//----------------------------- SYSTEM STATUS STRUCT ---------------------------------------------//
 #[derive(Debug, Default, PartialEq, Serialize, Deserialize)]
 pub struct SystemStatus {
     #[serde( skip_deserializing, skip_serializing_if = "should_skip_read_at" )]
     pub read_at: DateTime<Utc>,
-    pub governor: StatusValue,
-    pub temp1: StatusValue,
-    pub freq1: StatusValue,
-    pub gpu_temp: StatusValue,
-    pub zzz: StatusValue,
+    pub governor: SystemStatusValue,
+    pub temp1: SystemStatusValue,
+    pub freq1: SystemStatusValue,
+    pub gpu_temp: SystemStatusValue,
+    pub zzz: SystemStatusValue,
 }
 
 fn should_skip_read_at(read_at: &DateTime<Utc>) -> bool {
@@ -21,32 +21,32 @@ impl SystemStatus {
     pub fn new_with_auth(source: &SystemStatus) -> SystemStatus {
         SystemStatus {
             read_at: source.read_at,
-            governor:  StatusValue::new_with_auth(&source.governor),
-            temp1:  StatusValue::new_with_auth(&source.temp1),
-            freq1:  StatusValue::new_with_auth(&source.freq1),
-            gpu_temp:  StatusValue::new_with_auth(&source.gpu_temp),
-            zzz:  StatusValue::new_with_auth(&source.zzz),
+            governor:  SystemStatusValue::new_with_auth(&source.governor),
+            temp1:  SystemStatusValue::new_with_auth(&source.temp1),
+            freq1:  SystemStatusValue::new_with_auth(&source.freq1),
+            gpu_temp:  SystemStatusValue::new_with_auth(&source.gpu_temp),
+            zzz:  SystemStatusValue::new_with_auth(&source.zzz),
         }
     }
 }
 
 
-//------------------------------------------------------------------------------------------------//
+//----------------------------- SYSTEM STATUS VALUE STRUCT ---------------------------------------//
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct StatusValue {
+pub struct SystemStatusValue {
     //#[config(skip)]
     value: u64,
     pub lock: StatusEnum,
 }
 
-impl Default for StatusValue {
+impl Default for SystemStatusValue {
     fn default() -> Self {
         Self{ value: 0, lock: StatusEnum::Auth }
     }
 }
 
-impl StatusValue {
-    pub fn new_if_enabled<F>(read_fn: F, source: &StatusValue) -> StatusValue
+impl SystemStatusValue {
+    pub fn new_if_enabled<F>(read_fn: F, source: &SystemStatusValue) -> SystemStatusValue
         where F: FnOnce() -> u64,  {
         let lock = source.lock.clone();
         Self {
@@ -55,16 +55,16 @@ impl StatusValue {
         }
     }
 
-    fn new_with_auth(source: &StatusValue) -> StatusValue {
+    fn new_with_auth(source: &SystemStatusValue) -> SystemStatusValue {
         let lock = source.lock.clone();
-        StatusValue {
+        SystemStatusValue {
             value: if lock == StatusEnum::Auth { 0 } else { source.value },
             lock,
         }
     }
 }
 
-//------------------------------------------------------------------------------------------------//
+//----------------------------- SYSTEM STATUS VALUE ENUM -----------------------------------------//
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub enum StatusEnum {
     Disable,
@@ -74,7 +74,7 @@ pub enum StatusEnum {
 
 
 //------------------------------------------------------------------------------------------------//
-//                               TESTS                                                            //
+//                                          TESTS                                                 //
 //------------------------------------------------------------------------------------------------//
 #[cfg(test)]
 mod tests {
@@ -84,23 +84,23 @@ mod tests {
     fn test_system_status_new_with_auth() {
         let source = SystemStatus {
             read_at: Default::default(),
-            governor: StatusValue {
+            governor: SystemStatusValue {
                 value: 10,
                 lock: StatusEnum::Auth,
             },
-            temp1: StatusValue {
+            temp1: SystemStatusValue {
                 value: 20,
                 lock: StatusEnum::Auth,
             },
-            freq1: StatusValue {
+            freq1: SystemStatusValue {
                 value: 30,
                 lock: StatusEnum::Auth,
             },
-            gpu_temp: StatusValue {
+            gpu_temp: SystemStatusValue {
                 value: 40,
                 lock: StatusEnum::Auth,
             },
-            zzz: StatusValue {
+            zzz: SystemStatusValue {
                 value: 50,
                 lock: StatusEnum::Public,
             },
@@ -108,23 +108,23 @@ mod tests {
 
         let expected = SystemStatus {
             read_at: Default::default(),
-            governor: StatusValue {
+            governor: SystemStatusValue {
                 value: 0,
                 lock: StatusEnum::Auth,
             },
-            temp1: StatusValue {
+            temp1: SystemStatusValue {
                 value: 0,
                 lock: StatusEnum::Auth,
             },
-            freq1: StatusValue {
+            freq1: SystemStatusValue {
                 value: 0,
                 lock: StatusEnum::Auth,
             },
-            gpu_temp: StatusValue {
+            gpu_temp: SystemStatusValue {
                 value: 0,
                 lock: StatusEnum::Auth,
             },
-            zzz: StatusValue {
+            zzz: SystemStatusValue {
                 value: 50,
                 lock: StatusEnum::Public,
             },
@@ -137,30 +137,30 @@ mod tests {
 
     #[test]
     fn test_status_value_new_if_enabled_disable() {        // Test case 1: StatusEnum::Disable
-        let source = StatusValue {
+        let source = SystemStatusValue {
             value: 0,
             lock: StatusEnum::Disable,
         };
-        let result = StatusValue::new_if_enabled(|| 42, &source);
+        let result = SystemStatusValue::new_if_enabled(|| 42, &source);
         assert_eq!(result, source);
     }
     #[test]
     fn test_status_value_new_if_enabled_auth() {         // Test case 2: StatusEnum::Auth
-        let source = StatusValue {
+        let source = SystemStatusValue {
             value: 100,
             lock: StatusEnum::Auth,
         };
-        let result = StatusValue::new_if_enabled(|| 42, &source);
+        let result = SystemStatusValue::new_if_enabled(|| 42, &source);
         assert_eq!(result.value, 42);
         assert_eq!(result.lock, StatusEnum::Auth);
     }
     #[test]
     fn test_status_value_new_if_enabled_public() {        // Test case 3: StatusEnum::Public
-        let source = StatusValue {
+        let source = SystemStatusValue {
             value: 100,
             lock: StatusEnum::Public,
         };
-        let result = StatusValue::new_if_enabled(|| 42, &source);
+        let result = SystemStatusValue::new_if_enabled(|| 42, &source);
         assert_eq!(result.value, 42);
         assert_eq!(result.lock, StatusEnum::Public);
     }
